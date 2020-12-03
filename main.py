3e1723dca224e7f8a30d883ec532577c3744bace
francesco <francesco.zuppichini@gmail.com>
2020-12-03 18:41:45 +0100

    main

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests as r
+import json
+import sleep
+from random import choice
+from faker import Faker
+from faker.providers import internet
+
+URL = 'https://usb2.vf68.xyz/Home/Register/register.html'
+CODE = 2020101153485010
+
+counter = 0
+
+fake = Faker()
+fake.add_provider(internet)
+
+
+def get_user():
+    email, password = fake.email(), fake.password()
+    return email, password
+
+
+def spam():
+    email, password = get_user()
+    res = r.post(URL, data={
+        'email': email,
+        'password': password,
+        'passwords': password,
+        'code': 2020101153485010
+    }
+    )
+
+while True:
+    spam()
+    counter += 1
+    print(counter)
+    sleep(0.1)
